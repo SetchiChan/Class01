b2d7056f09d6f570a7af898c8c674699a310b21a
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2018-04-14 12:19:45 -0400

    Create Assignment2.js

---
+++ Assignment2.js
@@ -0,0 +1,18 @@
+public class PnNumber{
+  
+  public static void main(String [] args) {
+    
+    int number = 0;
+    
+    if (number > 0) {
+      System.out.println("This is a positive number");
+    } 
+    else if (number < 0) {
+      System.out.println("This is a negative number");
+    }
+    else if (number == 0) {
+      System.out.println("This is neither positive nor negative");
+    }
+  }
+}
+
