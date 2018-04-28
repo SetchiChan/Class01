36a13dbafd4037c03cb1a530bad8b23011afbca6
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2018-04-28 12:36:42 -0400

    Update Assignment4.js

--- Assignment4.js
+++ Assignment4.js
@@ -1,4 +1,4 @@
-//repeats 10 times name
+//repeats 10 times name (WHILE)
 public class counting {
 	public static void main(String[] args){
 	   
@@ -6,6 +6,21 @@ public class counting {
     while (count < 11) {
         System.out.println("Ethan");
 	   count++;
+	}
+    }
+}
+
+//FOR STATEMENT REPETITION
+public class counting {
+	public static void main(String[] args){
+	
+	for(int i = 0; i < 8; i++){
+	System.out.println("Words are bad");
+	
+	if(i == 3){
+	System.out.println("Words are good");} 
+	else if(i == 6){
+	System.out.println("More Words");}
 	}
     }
 }
