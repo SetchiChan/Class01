46ea1f80b682ae5a7142d6ae261968079e21af39
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2018-04-28 12:22:48 -0400

    Create Assignment4.js

---
+++ Assignment4.js
@@ -0,0 +1,11 @@
+//repeats 10 times name
+public class counting {
+	public static void main(String[] args){
+	   
+	int count = 1;
+    while (count < 11) {
+        System.out.println("Ethan");
+	   count++;
+	}
+    }
+}
