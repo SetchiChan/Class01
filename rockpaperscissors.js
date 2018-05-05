8c3777afa1cfff151db498cda9f02c9763bc77ca
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2018-05-05 13:00:02 -0400

    Create rockpaperscissors.js

---
+++ rockpaperscissors.js
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class RPS{
+  
+  public static void main(String [] args) throws IOException{
+  
+  System.out.println("===[Rock Paper Scissors]=== \n Welcome to a game of Rock, Paper Scissors ");
+  System.out.println("")
+  
+  }
+}
+
+//TESTING
+public class Math{
+	public static double piSquared() {
+      double pi = 3.14;
+      double piSq = pi * pi;
+      return piSq;
+
+	}
+  
+	public static int add(int num1, int num2) {
+      int sum = num1 + num2;
+      return sum; 
+
+	}
+  
+	public static void main(String [] args){
+      System.out.println(add(30,20));
+      System.out.println(piSquared());
+
+	}
+}
